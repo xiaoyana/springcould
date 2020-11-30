ed582da6c50239d3e8be3cae6710a144e4da7112
Zhangy <e_ayan@163.com>
2020-11-30 16:09:34 +0800

    1

---
+++ could-provider-payment8002/src/main/java/com/simulation/springcould/CouldProviderPayment8002.java
@@ -0,0 +1,17 @@
+package com.simulation.springcould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcould2
+ * @description:
+ * @author: zy
+ * @created: 2020/11/30 15:40
+ */
+@SpringBootApplication
+public class CouldProviderPayment8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(CouldProviderPayment8002.class,args);
+    }
+}
